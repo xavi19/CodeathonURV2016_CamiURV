943b5027c4f83beb4ac806e6f96cfac551679b72
Xavi Querol <xquerol19@gmail.com>
2016-02-14 10:54:09 +0100

    Activity Solicitut

--- CamiURV/app/src/main/java/com/cami/cat/camiurv/MainActivity.java
+++ CamiURV/app/src/main/java/com/cami/cat/camiurv/MainActivity.java
@@ -1,11 +1,19 @@
 package com.cami.cat.camiurv;
 
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -44,6 +52,35 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new MyWebViewClient());
         openURL();
+
+        NotificationCompat.Builder mBuilder =
+                (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.notification_icon)
+                        .setContentTitle("CamiURV")
+                        .setContentText("Sol·licitut de passatger!");
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, VeureSolicitut.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(VeureSolicitut.class);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+        int mId = 1234;
+        mNotificationManager.notify(mId, mBuilder.build());
     }
     /** Opens the URL in a browser */
     private void openURL() {
@@ -51,6 +88,21 @@ public class MainActivity extends AppCompatActivity {
         webView.requestFocus();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mostar_mapa:
+                Intent i = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(i);
+                break;
+        }
+        return true;
+    }
 
 
 }
